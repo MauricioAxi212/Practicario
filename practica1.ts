040436024099ed8e5d3f1ae64b2ed5ca8f473c44
MauricioAxi212 <mauricio.sanchezn@axity.com>
2025-02-11 15:23:56 -0600

    el primer commit

---
+++ practica1.ts
@@ -0,0 +1,23 @@
+/*  Descripción: Crea una clase Coche que herede de la clase Persona. 
+La clase Coche debe tener los siguientes atributos:
+
+marca (string)
+modelo (string)
+La clase Coche debe tener un método informacionCoche() que imprima la marca y el modelo. */
+
+class coche{
+    _marca: String
+    _modelo: String
+
+
+    constructor(marca:String, modelo: String){
+     this._marca = marca;
+     this._modelo = modelo;
+    }
+
+    informacionCoche(){
+        console.log(`La marca del carro es: ${this._marca} del modelo: ${this._modelo}.`)
+    }
+    
+}
+
